617d283f4b25fc351dce3fba32463838ea5cc2d6
gsngdavid <gsngdavid@gmail.com>
2023-07-19 16:14:15 +0200

    feat: My filter

--- 013. JavaScript Array Methods Implementation.js
+++ 013. JavaScript Array Methods Implementation.js
@@ -6,3 +6,12 @@ function myMap(cb) {
     return result;
 }
 
+function myFilter(cb) {
+    const result = this;
+
+    for(let i = 0; i < this.length; i++) {
+        if(!cb(this[i], i, this)) result.splice(i, 1);
+    }
+    return result;
+}
+
